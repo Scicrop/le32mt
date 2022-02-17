f14dc25da86114434f7f1b8cb34bafe93e26dc1f
Jose Damico <jose.damico@scicrop.com>
2022-02-17 18:18:24 -0300

    wifi + internet connection code

--- board_code/board_code.ino
+++ board_code/board_code.ino
@@ -1,39 +1,84 @@
 #include <Adafruit_BMP085.h>
 #include <DHT.h>;
-#define DHTPIN 11     
+#include <SoftwareSerial.h>
+#define DHTPIN 11
+#define LED_PIN 10
+#define M0 9
+#define M1 8       
 #define DHTTYPE DHT22   
 DHT dht(DHTPIN, DHTTYPE); 
 
-int LED_PIN = 10;
-float hum;  
-float temp;
+
+
 Adafruit_BMP085 bmp;
+SoftwareSerial loraSerial(3, 2); // TX, RX
+int hour = 3600*1000;
+int h_counter = 0;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
-  bmp.begin();
-  dht.begin();
-
+  pinMode(M0, OUTPUT);
+  pinMode(M1, OUTPUT);
+  sleep_lora();
+  Serial.begin(9600);
+  
+  loraSerial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);   
-  delay(1000);              
-  digitalWrite(LED_PIN, LOW);    
-  delay(1000);
-  Serial.print("Temperature = ");
-  Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
-  Serial.print("Pressure = ");
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
-  hum = dht.readHumidity();
-  temp= dht.readTemperature();
-  Serial.print("Humidity: ");
-    Serial.print(hum);
-    Serial.print(" %, Temp: ");
-    Serial.print(temp);
-    Serial.println(" Celsius");              
+  if(h_counter == 15 || h_counter == 0) digitalWrite(LED_PIN, HIGH);   
+  delay(500);              
+  if(h_counter == 15 || h_counter == 0) digitalWrite(LED_PIN, LOW);    
+  delay(500);
+  
+  
+  if(h_counter == 3600 || h_counter == 0){ 
 
+    bmp.begin();
+    dht.begin();
+   
+    String lora_msg_a = float_to_string(dht.readHumidity());
+    lora_msg_a.concat(";");
+    lora_msg_a.concat(float_to_string(dht.readTemperature()));
+    lora_msg_a.concat(";");
+    lora_msg_a.concat(bmp.readTemperature());
+    lora_msg_a.concat(";");
+    lora_msg_a.concat(bmp.readPressure());
+
+    wakeup_lora();
+    delay(500);
+    loraSerial.print(lora_msg_a);
+    transmit_led_status();
+    Serial.println(lora_msg_a);
+    delay(500);
+    sleep_lora();
+    if(h_counter != 0) h_counter = 1;
+  }              
+  h_counter++;
+}
+
+void sleep_lora(){
+  digitalWrite(M0, HIGH);
+  digitalWrite(M1, HIGH);
+}
+
+void wakeup_lora(){
+  digitalWrite(M0, LOW);
+  digitalWrite(M1, LOW);
+}
+
+void transmit_led_status(){
+
+  for(int i=0; i<10; i++){
+    digitalWrite(LED_PIN, HIGH);   
+    delay(20);
+    digitalWrite(LED_PIN, LOW);   
+    delay(20);                  
+  }
+  
+}
+
+String float_to_string(float source){
+  char buffer[5];
+  return dtostrf(source, 1, 4, buffer);
 }
---
+++ receiver_code/receiver_code.ino
@@ -0,0 +1,108 @@
+#include <SoftwareSerial.h>
+
+#include <BearSSLHelpers.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiGratuitous.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266HTTPClient.h>
+
+SoftwareSerial loraSerial(4, 5); // TX, RX
+WiFiClient client;
+const char* ssid = "Subcasa";
+const char* password = "laika2004";
+int LED_LORA = 15;
+int LED_WIFI = 13;
+int LED_INET = 12;
+int result = -1;
+
+int time_check = 270000;
+
+int counter_lora = time_check;
+int counter_wifi = time_check;
+int counter_inet = time_check;
+
+const char* host = "http://scicrop.com/trihum";
+char urlout[255];
+HTTPClient http;
+int httpCode = 0; 
+
+void setup() {
+ 
+  pinMode(LED_LORA, OUTPUT);
+  pinMode(LED_WIFI, OUTPUT);
+  pinMode(LED_INET, OUTPUT);
+  Serial.begin(9600);
+  loraSerial.begin(9600);
+  Serial.println("booting...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting wifi...");
+  }
+  Serial.println("Wifi Connected.");
+  counter_wifi = 0;  
+  result = loraSerial.available();
+  Serial.println(result);
+
+
+}
+
+void loop() {
+ 
+ if(result == 0){
+    String input = loraSerial.readString();
+    int input_len = input.length();
+    int valid_input = -1;
+    if(input_len > 3) valid_input = 0; 
+    if(valid_input == 0){
+      Serial.println(input); 
+      counter_lora = 0;
+      strcpy(urlout, host);
+      strcat(urlout, "/?data=");
+      char buffered_char[30];
+      input.toCharArray(buffered_char, 50);
+      
+      strcat(urlout, buffered_char);
+      if (WiFi.status() == WL_CONNECTED) { 
+        counter_wifi = 0;
+        http.begin(client, urlout); 
+        httpCode = http.GET();
+        Serial.println(urlout);
+        Serial.println(httpCode);
+        if (httpCode > 0) { 
+          String payload = http.getString();  
+          Serial.println(payload);
+          counter_inet = 0;            
+        }
+        http.end();   
+      }        
+    }
+    if(counter_lora <= time_check) digitalWrite(LED_LORA, HIGH);
+    if(counter_wifi <= time_check) digitalWrite(LED_WIFI, HIGH);
+    if(counter_inet <= time_check) digitalWrite(LED_INET, HIGH); 
+    delay(10);
+    if(counter_lora <= time_check) digitalWrite(LED_LORA, LOW);
+    if(counter_wifi <= time_check) digitalWrite(LED_WIFI, LOW);
+    if(counter_inet <= time_check) digitalWrite(LED_INET, LOW);
+    delay(10);
+    if(counter_lora > time_check){ digitalWrite(LED_LORA, HIGH); Serial.println("failed @ lora connection!"); }
+    if(counter_wifi > time_check){ digitalWrite(LED_WIFI, HIGH); Serial.println("failed @ wifi connection!"); }
+    if(counter_inet > time_check){ digitalWrite(LED_INET, HIGH); Serial.println("failed @ internet connection");} 
+    counter_lora++;
+    counter_wifi++;
+    counter_inet++;
+  }else{
+    digitalWrite(LED_LORA, HIGH);
+    Serial.println("failed @ loraSerial.available()");
+    delay(2000); 
+    digitalWrite(LED_LORA, LOW);
+    delay(2000);
+  }
+}
